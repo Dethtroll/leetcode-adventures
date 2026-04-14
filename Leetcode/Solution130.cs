e8c86fc3ede6b37ced7696c58dc2b309d4b9839d
Aleksey Zhuravlev <aleksey.zhuravlev@singularis-lab.com>
2026-04-14 15:51:52 +0300

    [130][695] Added

---
+++ Leetcode/Solution130.cs
@@ -0,0 +1,57 @@
+﻿namespace Leetcode;
+
+//time: O(m*n)
+//mem: O(m*n)
+public class Solution130
+{
+    public void Solve()
+    {
+        Solve([['X', 'X', 'X', 'X'], ['X', 'O', 'O', 'X'], ['X', 'X', 'O', 'X'], ['X', 'O', 'X', 'X']]);
+    }
+
+    public void Solve(char[][] board)
+    {
+        var m = board.Length;
+        var n = board[0].Length;
+        var visited = new bool[m, n];
+
+        //помечаем крайние столбцы
+        for (int i = 0; i < m; i++)
+        {
+            Dfs(board, i, 0, visited);
+            Dfs(board, i, n - 1, visited);
+        }
+
+        //помечаем крайние строки
+        for (int j = 0; j < n; j++)
+        {
+            Dfs(board, 0, j, visited);
+            Dfs(board, m - 1, j, visited);
+        }
+
+        for (var i = 1; i < m - 1; i++)
+            for (var j = 1; j < n - 1; j++)
+                if (!visited[i, j])
+                    board[i][j] = 'X';
+    }
+    
+    private void Dfs(char[][] board, int row, int col, bool[,] visited)
+    {
+        if (row < 0 || board.Length <= row || col < 0 || board[row].Length <= col)
+            return;
+
+        if (visited[row, col])
+            return;
+
+        visited[row, col] = true;
+        if (board[row][col] == 'X')
+            return;
+
+        board[row][col] = 'X';
+
+        Dfs(board, row - 1, col, visited);
+        Dfs(board, row + 1, col, visited);
+        Dfs(board, row, col - 1, visited);
+        Dfs(board, row, col + 1, visited);
+    }
+}
---
+++ Leetcode/Solution695.cs
@@ -0,0 +1,49 @@
+﻿namespace Leetcode;
+
+public class Solution695
+{
+    public void Solve()
+    {
+        var res = MaxAreaOfIsland([
+            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+        ]);
+        Console.WriteLine(res);
+    }
+    public int MaxAreaOfIsland(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+        var maxArea = 0;
+        var visited = new HashSet<(int, int)>();
+
+        int Bfs(int row, int col)
+        {
+            if (row < 0 || grid.Length <= row || col < 0 || grid[row].Length <= col)
+                return 0;
+            if (grid[row][col] == 0)
+                return 0;
+            if (!visited.Add((row, col)))
+                return 0;
+
+            return 1
+                   + Bfs(row - 1, col)
+                   + Bfs(row + 1, col)
+                   + Bfs(row, col - 1)
+                   + Bfs(row, col + 1);
+        }   
+        
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == 1)
+                    maxArea = Math.Max(maxArea, Bfs(i, j));
+
+        return maxArea;
+    }
+}
