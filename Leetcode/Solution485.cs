d59fc821b86c25cdb696881946e69ef70f35f0a3
Aleksey Zhuravlev <aleksey.zhuravlev@singularis-lab.com>
2026-04-14 23:33:52 +0300

    [485] added

---
+++ Leetcode/Solution485.cs
@@ -0,0 +1,27 @@
+﻿namespace Leetcode;
+
+public class Solution485
+{
+    public void Solve()
+    {
+        var res = FindMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]);
+        Console.WriteLine(res);
+    }
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        var max = 0;
+
+        var len = 0;
+        foreach (var num in nums)
+        {
+            if (num == 1)
+            {
+                len++;
+                max = Math.Max(max, len);
+            }
+            else
+                len = 0;
+        }
+
+        return max;
+    }
+}
