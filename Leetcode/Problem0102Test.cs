5a5636ef44aaaca25a3a4d01fcf763139de62660
Aleksey Zhuravlev <aleksey.zhuravlev@singularis-lab.com>
2026-04-17 19:14:51 +0300

    [145][102] Added

---
+++ Leetcode/Problem0102Test.cs
@@ -0,0 +1,73 @@
+﻿using Leetcode.Common;
+using Leetcode.Helpers;
+
+namespace Leetcode;
+
+public class Problem0102Test
+{
+    private readonly Solution102 _solution = new();
+
+    private void BaseTest(int?[] input, List<List<int>> expected)
+    {
+        var tree = input.ToTree();
+        
+        var result = _solution.LevelOrder(tree!);
+        
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void LongTest()
+    {
+        int?[] input = [3, 9, 20, null, null, 15, 7];
+        List<List<int>> result = [[3], [9, 20], [15, 7]]; 
+        
+        BaseTest(input, result);
+    }
+
+    [Fact]
+    public void ShortTest()
+    {
+        int?[] input = [1];
+        List<List<int>> result = [[1]]; 
+        
+        BaseTest(input, result);
+    }
+
+    [Fact]
+    public void EmptyTest()
+    {
+        int?[] input = [];
+        List<List<int>> result = []; 
+        
+        BaseTest(input, result);
+    }
+}
+
+public class Solution102 {
+    public IList<IList<int>> LevelOrder(TreeNode? root)
+    {
+        var result = new List<IList<int>>();
+        if (root == null) {
+            return result;
+        }
+
+        Build(root, 0, result);
+        
+        return result;
+    }
+
+    private void Build(TreeNode node, int level, List<IList<int>> result)
+    {
+        if (level == result.Count)
+        {
+            result.Add(new List<int>());
+        }
+        result[level].Add(node.val);
+        
+        if(node.left != null)
+            Build(node.left, level + 1, result);
+        if (node.right != null)
+            Build(node.right, level + 1, result);
+    }
+}
---
+++ Leetcode/Problem0145Test.cs
@@ -0,0 +1,87 @@
+﻿using Leetcode.Common;
+using Leetcode.Helpers;
+
+namespace Leetcode;
+
+public class Problem0145Test
+{
+    private readonly Solution145 _solution = new();
+
+    private void BaseTest(int?[] input, int[] expected)
+    {
+        var tree = input.ToTree();
+        
+        var resultStack = _solution.PostorderTraversal(tree);
+        var resultRecursive = _solution.PostorderTraversalRecursive(tree);
+        
+        Assert.Equal(expected, resultStack.ToArray());
+        Assert.Equal(expected, resultRecursive.ToArray());
+    }
+
+    [Fact]
+    public void LongTest()
+    {
+        BaseTest([1, 2, 3, 4, 5, null, 8, null, null, 6, 7, null, null, 9], [4, 6, 7, 5, 2, 9, 8, 3, 1]);
+    }
+    [Fact]
+    public void ShortTest()
+    {
+        BaseTest([ 1, null, 2, null, null, 3 ], [ 3, 2, 1 ]);
+    }
+    [Fact]
+    public void EmptyTest()
+    {
+        BaseTest([], []);
+    }
+    [Fact]
+    public void SingleTest()
+    {
+        BaseTest([1], [1]);
+    }
+}
+
+public class Solution145 {
+    public IList<int> PostorderTraversal(TreeNode? root) {
+        var result = new List<int>();
+        if (root == null)
+            return result;
+
+        var stack = new Stack<(TreeNode node, bool processed)>();
+        stack.Push((root, false));
+
+        while (stack.Any())
+        {
+            var data = stack.Pop();
+            if (data.processed)
+            {
+                result.Add(data.node.val);
+                continue;
+            }
+
+            stack.Push((data.node, true));
+            if (data.node.right != null)
+                stack.Push((data.node.right, false));
+            if (data.node.left != null)
+                stack.Push((data.node.left, false));
+        }
+        
+        return result;
+    }
+    public IList<int> PostorderTraversalRecursive(TreeNode? root) {
+        var result = new List<int>();        
+        
+        FillNode(root, result);
+        
+        return result;
+    }
+
+    private void FillNode(TreeNode? node, List<int> result)
+    {
+        if (node == null)
+            return;
+        
+        FillNode(node.left, result);
+        FillNode(node.right, result);
+        result.Add(node.val);
+    }
+}
